364b6d3668c8b76e6b4761c7fbbea9e1987d6501
kimchieater <jasonj19276@gmail.com>
2023-07-31 18:55:19 +1000

    created a contact section

--- script.js
+++ script.js
@@ -25,3 +25,54 @@ function removeActiveClasses() {
   });
 }
 // my Work ends
+// contact starts
+//get data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+//validate data
+function validateForm() {
+  clearMessages();
+  let errorFlag = false;
+
+  if (nameInput.value.length < 1) {
+    errorNodes[0].innerText = "Name cannot be blank";
+    nameInput.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if (!emailIsValid(email.value)) {
+    errorNodes[1].innerText = "Invailid email address";
+    email.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if (message.value.length < 1) {
+    errorNodes[2].innerText = "Don't be shy!";
+    message.classList.add("error-border");
+    errorFlag = true;
+  }
+
+  if (!errorFlag) {
+    success.innerText = "Thank you! I'll be in contact soon!";
+  }
+}
+
+function clearMessages() {
+  for (let i = 0; i < errorNodes.length; i++) {
+    errorNodes[i].innerText = "";
+  }
+  success.innerText = "";
+  nameInput.classList.remove("error-border");
+  email.classList.remove("error-border");
+  message.classList.remove("error-border");
+}
+
+function emailIsValid(email) {
+  let pattern = /\S+@\S+\.\S+/;
+  return pattern.test(email);
+}
+// contact ends
